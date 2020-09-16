235742c5c9dc45c20f898be35c13058de6093a30
Caio <caiojulian@hotmail.com>
2020-09-16 11:25:59 -0400

    add atividade

--- src/programa.ts
+++ src/programa.ts
@@ -26,4 +26,25 @@ export class Programa extends Contract {
 
         await ctx.stub.putState(id, Buffer.from(JSON.stringify(beneficio)));
     }
+
+    public async criarAtividade(ctx: Context, id: string, atividade: any){
+        // validar os atributos da atividade
+        await ctx.stub.putState(id, Buffer.from(JSON.stringify(atividade)));
+    }
+
+    public async consultarAtividade(ctx: Context, id: string): Promise<string>{
+        const atividade = await ctx.stub.getState(id); // get the id from chaincode state
+        if (!atividade || atividade.length === 0) {
+            throw new Error(`${id} does not exist`);
+        }
+        console.log(JSON.parse(JSON.stringify(atividade)));
+        return JSON.parse(JSON.stringify(atividade));
+    }
+
+    public async inativarAtividade(ctx: Context, id: string) {
+        let atividade = JSON.parse(await this.consultarAtividade(ctx, id));
+        atividade.ativo = false;
+
+        await ctx.stub.putState(id, Buffer.from(JSON.stringify(atividade)));
+    }
 }
